cb85f1434db3327ec8c394f6757cc3c1f1b9e01a
Yyassin <you.ayassin@gmail.com>
2020-06-06 16:47:14 -0400

    Chat & message styling, new windows titlebar (icons)

--- client/src/components/Titlebar/Titlebar-legacy.js
+++ client/src/components/Titlebar/Titlebar-legacy.js
@@ -60,35 +60,23 @@ const Titlebar = (props) => {
                 </div>
                 <div className="Title-Bar__section-windows-control">
                     <div
-                        className="section-windows-control_box-min" onClick={minimizeHandler}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" strokeLinejoin="round"
-                        className={isActive ? 'minimize-active_logo' : 'minimize-inactive_logo'}>
-                            <rect x="0" y="50%" width="10.2" height="1" />
-                            </svg>
+                        className="section-windows-control_box">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" strokeLinejoin="round"><circle onClick={minimizeHandler} className={isActive ? 'minimize-active_logo' : 'minimize-inactive_logo'} cx="11.6" cy="11.6" r="11.4" /></svg>
                     </div>
                     {isMaximized ?
                         <div
-                            className="section-windows-control_box-unmax" onClick={unmaximizeHandler}>
-                            <svg xmlns="http://www.w3.org/2000/svg"  width="10" height="10" viewBox="0 0 10 10" strokeLinejoin="round"
-                            className={isActive ? 'maximize-active_logo' : 'maximize-inactive_logo'}>
-                                <path d="M2.1,0v2H0v8.1h8.2v-2h2V0H2.1z M7.2,9.2H1.1V3h6.1V9.2z M9.2,7.1h-1V2H3.1V1h6.1V7.1z" />
-                            </svg>
+                            className="section-windows-control_box">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" strokeLinejoin="round"><circle onClick={unmaximizeHandler} className={isActive ? 'unmaximize-active_logo' : 'unmaximize-inactive_logo'} cx="11.6" cy="11.6" r="11.4" /></svg>
                         </div>
                         :
                         <div
-                            className="section-windows-control_box-max" onClick={maximizeHandler}>
-                            <svg xmlns="http://www.w3.org/2000/svg"  width="10" height="10" viewBox="0 0 10 10" strokeLinejoin="round"
-                            className={isActive ? 'maximize-active_logo' : 'maximize-inactive_logo'}>
-                                <path d="M0,0v10h10V0H0z M9,9H1V1h8V9z" />
-                            </svg>   
+                            className="section-windows-control_box">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" strokeLinejoin="round"><circle onClick={maximizeHandler} className={isActive ? 'maximize-active_logo' : 'maximize-inactive_logo'} cx="11.6" cy="11.6" r="11.4" /></svg>
                         </div>
                     }
                     <div
-                        className="section-windows-control_box-close" onClick={closeHandler}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" strokeLinejoin="round" 
-                        className={isActive ? 'close-active_logo' : 'close-inactive_logo'}>
-                            <polygon points="10.2,0.7 9.5,0 5.1,4.4 0.7,0 0,0.7 4.4,5.1 0,9.5 0.7,10.2 5.1,5.8 9.5,10.2 10.2,9.5 5.8,5.1" />
-                        </svg>
+                        className="section-windows-control_box">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" strokeLinejoin="round"><circle onClick={closeHandler} className={isActive ? 'close-active_logo' : 'close-inactive_logo'} cx="11.6" cy="11.6" r="11.4" /></svg>
                     </div>
                 </div>
                 <div
